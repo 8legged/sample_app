c52fcf5f02cf4e03063bd7a0a7e83f5d3042fb9b
8legged <jason.niemi@live.com>
2013-08-19 22:49:16 -0700

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,23 @@
 include ApplicationHelper
+
+# With the code in Listing 8.34, we can write
+# it { should have_error_message('Invalid') }
+# and
+# describe "with valid information" do
+#   let(:user) { FactoryGirl.create(:user) }
+#   before { valid_signin(user) }
+#   .
+#   .
+#   .
+
+def valid_signin(user)
+  fill_in "Email", 		with: user.email
+  fill_in "Password", 	with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+  	expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
