c10f256a70902591bba7a6690e4b42a8d5c52aab
8legged <jason.niemi@live.com>
2013-08-23 01:54:30 -0700

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,11 +52,20 @@ describe "User Pages" do
 
   describe "profile page" do
 	let(:user) { FactoryGirl.create(:user) }
+  let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+  let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 	before { visit user_path(user) }
 
 	it { should have_content(user.name) }
 	it { should have_title(user.name) }
+
+  describe "microposts" do
+    it { should have_content(m1.content) }
+    it { should have_content(m2.content) }
+    it { should have_content(user.microposts.count) }
   end
+end
 
   describe "signup page" do
 	before { visit signup_path }
@@ -105,6 +114,15 @@ describe "User Pages" do
     before do
       sign_in user
       visit edit_user_path(user)
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before { patch user_path(user), params }
+      specify { expect(user.reload).not_to be_admin }
     end
 
     describe "page" do
